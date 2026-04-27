f4e1cf37289b44bcce33b6e3f7522397c8d897e9
S KARTHIK <skarthik.link@gmail.com>
2026-04-27 17:07:42 +0530

    feat: initialize project skeleton with folder structure, Main class and pom.xml

---
+++ src/test/java/com/pm/hospitalmanagementsystemspringbootjwt/HospitalManagementSystemSpringbootJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pm.hospitalmanagementsystemspringbootjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalManagementSystemSpringbootJwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
